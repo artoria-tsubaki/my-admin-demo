caae697211505784840e1602ffdf31bec49dcc3f
pycd <1248353039@qq.com>
2023-01-06 18:09:11 +0800

    commit

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,12 @@
 <template>
-	<div class="home card">
-		<img class="home-bg" src="@/assets/images/welcome.png" alt="welcome" />
-	</div>
+  <div class="home card">
+    <!-- 首页 -->
+    <img class="home-bg" src="@/assets/images/welcome.png" alt="welcome" />
+  </div>
 </template>
 
 <script setup lang="ts" name="home"></script>
 
 <style scoped lang="scss">
-@import "./index.scss";
+@import './index.scss';
 </style>
