7b435ff1df3e814194d6ea33dd245ff0b4fc7592
pycd <1248353039@qq.com>
2023-01-17 17:54:33 +0800

    draggable form page complete

---
+++ src/views/assembly/draggable/index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts" name="Draggable">
+import { ref } from 'vue'
+import draggable from 'vuedraggable/src/vuedraggable'
+
+let gridList = ref([
+  { id: 1, num: 1 },
+  { id: 2, num: 2 },
+  { id: 3, num: 3 },
+  { id: 4, num: 4 },
+  { id: 5, num: 5 },
+  { id: 6, num: 6 },
+  { id: 7, num: 7 },
+  { id: 8, num: 8 },
+  { id: 9, num: 9 }
+])
+</script>
+
+<template>
+  <draggable class="card grid-container" v-model="gridList" item-key="id" animation="300" chosenClass="chosen" forceFallback="true">
+    <template #item="{ element }">
+      <div :class="'item' + ' ' + 'item-' + element.num">
+        {{ element.num }}
+      </div>
+    </template>
+  </draggable>
+</template>
+
+<style lang="scss" scoped>
+@import './index.scss';
+</style>
